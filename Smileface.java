4b3caeda481c1e292bc86e80bea1ddd7064a6692
Menelik <41522285+menelik-g@users.noreply.github.com>
2019-10-25 17:05:27 -0400

    Just faces in java

---
+++ Smileface.java
@@ -0,0 +1,29 @@
+/** Menelik Gebremariam 09/30/19
+Simple face making project with Java, Keep going, you can do this!
+*/
+
+public class Smileface
+{
+   public static void main(String[] args)
+   {
+      System.out.println(" \\\\\\\\\\\\//////");
+      System.out.println(" |  _     _ |");
+      System.out.println(" *     ^    *");
+      System.out.println(" |__________|");
+      System.out.println("  |       |");
+      System.out.println("  | U   U |    Oh boy, I sure hope that this code compiles or else ill cry");
+      System.out.println("  |       |");
+      System.out.println("  \"\"\"\"\"\"\"\"\" ");
+      System.out.println("                                          ////////////");
+      System.out.println("*\n                             !!!!      | 0    0  |");
+      System.out.println("**\n                                      *____^____*");
+      System.out.println("***\n                                      |       |");
+      System.out.println("**\n                                       |       |");
+      System.out.println("*****\n                                    |       |");
+      System.out.println("***\n                                    \"+++++++\"");
+      System.out.println("######Beep##Boop##Boop######     911, Whats your emergency?");
+      System.out.println("\"Oh god my isnt running oh gosh its giving me a error oh no the escape sequence got me!\"");
+      String \u03C0 = "\u03C0";
+      System.out.println(\u03C0);
+    }
+}
